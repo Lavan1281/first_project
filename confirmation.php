f547b1501ad679744b01b9cdf6dcb6d449d2e7cd
Lavanya <alavanya1281@gmail.com>
2023-09-29 11:29:29 +0530

    Git project

---
+++ confirmation.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Confirmation Page</title>
+</head>
+<body>
+    <h1>Confirmation Page</h1>
+    <p>Thank you for your submission. Your request has been confirmed.</p>
+</body>
+</html>
